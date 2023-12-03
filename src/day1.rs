66b3408342ca4477b5d3e22ec2106da97cb9961c
Alina Hagan <alina-hagan@outlook.com>
2023-12-03 19:38:45 +0100

    redo, remove the printout

--- src/day1.rs
+++ src/day1.rs
@@ -4,17 +4,19 @@ pub fn solve_part1(input: &str) -> u32 {
     let lines = input.split( "\n" );
     let mut acc: u32 = 0;
     for line in lines{
-        println!( "{}", line );
         let mut nums = line.chars()
             .into_iter()
-            .filter( |&c| !c.is_alphabetic() )
+            .filter( |&c| c.is_digit( 10 ) )
             .map( |x| (x as u32)-48 );
-        let mut temp =  10*nums.nth( 0 ).unwrap();
+        let mut temp =  nums.nth( 0 ).unwrap();
         match nums.last(){
-            Some( value ) => temp += value,
-            None => temp += temp/10,
+            Some( value ) => temp = temp*10 + value,
+            None => temp += temp*10,
         }
         acc += temp;
     }
     acc
 }
+
+//#[aoc(day1, part2)]
+//pub fn solve_part1(input: &str) -> u32 {
---
+++ src/day2.rs
@@ -0,0 +1,8 @@
+
+#[aoc(day2, part1)]
+pub fn solve_part1(input: &str) -> u32 {
+    0
+}
+
+//#[aoc(day1, part2)]
+//pub fn solve_part1(input: &str) -> u32 {
